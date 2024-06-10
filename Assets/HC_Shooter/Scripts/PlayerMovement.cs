61d8fef700676a7ca89d09145657a51eab53a619
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-06-10 06:49:50 +0300

    .

--- Assets/HC_Shooter/Scripts/PlayerMovement.cs
+++ Assets/HC_Shooter/Scripts/PlayerMovement.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField]
+    private float moveSpeed;
+
+
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+
+    private void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    }
 }
