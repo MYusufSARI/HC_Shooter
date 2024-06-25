d0bf0293501bc62e76a8d0596b65907abca5e693
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-06-25 03:46:20 +0300

    Update PlayerDetection.cs

--- Assets/HC_Shooter/Scripts/PlayerDetection.cs
+++ Assets/HC_Shooter/Scripts/PlayerDetection.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
 {
-   
+
+    [Header(" Settings ")]
+    [SerializeField]
+    private float detectionRadius;
+
+    private void Update()
+    {
+        DetectStuff();
+    }
+
+
+    private void DetectStuff()
+    {
+        Collider[] detectedObjects = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        foreach (Collider collider in detectedObjects)
+        {
+            Debug.Log(collider.name);
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
 }
