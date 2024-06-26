d17802d21a8d893264ef4e000492f73d7d77be67
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-06-27 02:17:56 +0300

    Update PlayerShooter.cs

--- Assets/HC_Shooter/Scripts/PlayerShooter.cs
+++ Assets/HC_Shooter/Scripts/PlayerShooter.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class PlayerShooter : MonoBehaviour
 {
     [Header(" Elements ")]
+    [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private GameObject shootingLine;
+    [SerializeField] private Transform bulletSpawnPosition;
+
+
+    [Header(" Settings ")]
+    private bool canShoot;
 
 
 
@@ -16,7 +22,6 @@ public class PlayerShooter : MonoBehaviour
         PlayerMovement.onEnteredWarzone += EnteredWarzoneCallback;
         PlayerMovement.onExitedWarzone += ExitedWarzoneCallback;
     }
-
 
 
     private void OnDestroy()
@@ -32,15 +37,41 @@ public class PlayerShooter : MonoBehaviour
     }
 
 
+    private void Update()
+    {
+        if (canShoot)
+        {
+            ManageShooting();
+        }
+    }
+
+
+    private void ManageShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
+    }
+
+
     private void EnteredWarzoneCallback()
     {
         SetShootingLineVisibility(true);
+        canShoot = true;
     }
 
 
     private void ExitedWarzoneCallback()
     {
         SetShootingLineVisibility(false);
+        canShoot = false;
     }
 
 
