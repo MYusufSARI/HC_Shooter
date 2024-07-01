fa81df8e7f5ad0c18be524e469a63904ec31a8c6
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-07-01 21:38:33 +0300

    Update LevelManager.cs

--- Assets/HC_Shooter/Scripts/Managers/LevelManager.cs
+++ Assets/HC_Shooter/Scripts/Managers/LevelManager.cs
@@ -21,6 +21,7 @@ public class LevelManager : MonoBehaviour
     {
         LoadData();
 
+        if(!preventSpawning)
         SpawnLevel();
 
         GameManager.onGameStateChanged += GameStateChangedCallback;
